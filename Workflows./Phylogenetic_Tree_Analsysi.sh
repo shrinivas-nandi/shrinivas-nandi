50b61323c14732d5f23afaac7540a82d64a4af75
Shrinivas Nandi <124947305+shrinivas-nandi@users.noreply.github.com>
2024-01-08 12:41:05 -0500

    Update Phylogenetic_Tree_Analsysi.sh

--- Workflows./Phylogenetic_Tree_Analsysi.sh
+++ Workflows./Phylogenetic_Tree_Analsysi.sh
@@ -77,3 +77,56 @@ for file in "$input_dir"/*.fasta; do
 
     echo "Processed: $filename"
 done
+
+
+
+####### 
+#######
+# This bit of code here is done on R. 
+# Effectively a loop for plotting out all the tree files. 
+library(ape)
+
+# Function to assign colors based on species names
+assign_tip_color <- function(species_name) {
+  if (grepl("KR01", species_name)) {
+    return("blue")
+  } else if (grepl("^g", species_name)) {
+    return("red")
+  } else if (grepl("CCAC0185", species_name)) {
+    return("green")
+  } else {
+    return("orange")  # Default color for other species
+  }
+}
+
+# Function to plot and save tree as PDF
+plot_and_save_tree <- function(tree_file_path, output_pdf_path) {
+  # Read the tree
+  tree <- read.tree(tree_file_path)
+  
+  # Assign tip colors
+  tip_colors <- sapply(tree$tip.label, assign_tip_color)
+  
+  # Plot the tree with adjusted width and height
+  pdf(output_pdf_path, width = 20, height = 12)  # Adjust the width and height as needed
+  plot(tree, edge.width = 1.5, cex = 1, tip.color = tip_colors)
+  nodelabels(tree$node.label, node = 2:(tree$Nnode + Ntip(tree)), adj = c(1, -0.2), frame = "none", cex = 0.5)
+  dev.off()
+}
+
+# Directory containing tree files
+tree_directory <- "/Users/shrinivas/Desktop/Paulinella/P_ovalis_Metagenome/Tree_plot/"
+
+# List all tree files in the directory
+tree_files <- list.files(tree_directory, pattern = "\\.treefile$", full.names = TRUE)
+
+# Loop through each tree file
+for (tree_file in tree_files) {
+  # Generate output PDF path based on the input tree file name
+  output_pdf_path <- paste0(tools::file_path_sans_ext(tree_file), "_colored.pdf")
+  
+  # Plot and save the tree with adjusted width and height
+  plot_and_save_tree(tree_file, output_pdf_path)
+}
+
+
