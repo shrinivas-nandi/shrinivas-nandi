fe0c130ae283bf53fd83fd7b0b5bb2ab548657f8
Shrinivas Nandi <124947305+shrinivas-nandi@users.noreply.github.com>
2024-02-29 11:25:47 -0500

    Update Move_file_one_directory_to_another.sh

--- Move_file_one_directory_to_another.sh
+++ Move_file_one_directory_to_another.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Source directory containing all files
-source_directory="/scratch/shrinivas/Paulinella_Metagenome/Gene_duplication_2/Orthogroup_Sequences"
+# Source directory containing all files with .fa extension
+source_directory="/scratch/shrinivas/Paulinella_Metagenome/Oct24_2023/Outgroup_included_analysis/OrthoFinder/Results_Oct24_1/Orthogroup_Sequences"
 
 # Destination directory to move files to
-destination_directory="/scratch/shrinivas/Paulinella_Metagenome/Gene_duplication_2/non-target_OG"
+destination_directory="/scratch/shrinivas/Paulinella_Metagenome/HGT/candidate_OG"
 
 # Txt file containing the list of filenames to move
-files_to_move_txt="Non_target_orthogroup.txt"
+files_to_move_txt="files2move.txt"
 
 # Ensure destination directory exists
 mkdir -p "$destination_directory"
 
 # Move files listed in the txt file to the destination directory
 while IFS= read -r filename; do
-  if [ -e "$source_directory/$filename" ]; then
-    mv "$source_directory/$filename" "$destination_directory/"
-    echo "Moved: $filename"
-  else
-    echo "File not found: $filename"
-  fi
+    filename_with_extension="$filename.fa"
+    if [ -e "$source_directory/$filename_with_extension" ]; then
+        mv "$source_directory/$filename_with_extension" "$destination_directory/"
+        echo "Moved: $filename_with_extension"
+    else
+        echo "File not found: $filename_with_extension"
+    fi
 done < "$files_to_move_txt"
-
 
