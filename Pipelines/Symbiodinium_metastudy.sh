c5d45736f82c456625747ea6d54f99cf605e3c4a
Shrinivas Nandi <124947305+shrinivas-nandi@users.noreply.github.com>
2024-03-18 13:22:55 -0400

    Create Symbiodinium_metastudy.sh

---
+++ Pipelines/Symbiodinium_metastudy.sh
@@ -0,0 +1,24 @@
+Symbiodinium_metastudy.sh
+
+
+'''
+Downloading the raw reads 
+Utilize kingfisher wwood to do this.
+For trial puprose we will use this PRJNA342240
+'''
+# lets get the SRA files associated with this 
+module run Kingfisher_v0.4.1-rev1 kingfisher annotate --bioprojects PRJNA342240 -o PRJNA342240.tsv -f tsv
+cut -f1 your_file.tsv > PRJ.txt # to get only the run file 
+
+# script to download all SRA associated
+#!/bin/bash
+
+# Assuming your list is stored in a file named "input_list.txt"
+input_list="input_list.txt"
+
+# Loop through each item in the list
+while IFS= read -r line; do
+    # Run the module command with the specified arguments
+    module run Kingfisher_v0.4.1-rev1 kingfisher get -r "$line" -m ena-ascp aws-http prefetch
+done < "$input_list"
+
